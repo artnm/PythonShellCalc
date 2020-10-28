de28ea2632c01e9ec5fbd0ff9bc02aaf43ebf814
aonor <justsomechannel261@gmail.com>
2020-10-27 22:19:12 -0500

    readme and calc
    retrying this with a singular branch :/

---
+++ README.md
@@ -0,0 +1,19 @@
+my python shell calculator
+
+
+current operations are:
+
+addition <br />
+subtraction <br />
+multiplication <br />
+division <br />
+exponents <br />
+square root <br />
+nth root <br />
+logarithm <br />
+modulo <br />
+sine <br />
+cosine <br />
+tangent <br />
+cotangent <br />
+secant
---
+++ calc.py
@@ -0,0 +1,122 @@
+import math
+
+import mpmath
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def exponent(x, y):
+    return (x ** y)
+
+def sqrt(x):
+    return(x)**(1/2)
+
+def nthroot(x, y):
+    return pow(y,(1/x))
+
+def log(x, y):
+    return math.log(y, x)
+
+def modulo(x, y):
+    return x % y
+
+def sine(x):
+    return math.sin(x)
+
+def cosine(x):
+    return math.cos(x)
+
+def tangent(x):
+    return math.tan(x)
+
+def cotangent(x):
+    return mpmath.cot(x)
+
+def secant(x):
+    return mpmath.sec(x)
+
+print("Select an operation:\n")
+print("   addition\n   subtraction\n   multiplication\n   division\n   exponents\n   square root\n   nth root\n   logarithm\n   modulo\n   sine\n   cosine\n   tangent\n   cotangent\n   secant\n")
+
+
+while True:
+    # User input
+    choice = input("Enter operation's name: ")
+
+
+    #2 choice ###################################
+    # Check if choice is one of the options
+    if choice in ("addition", "subtraction", "multiplication", "division", "exponents", "nth root", "logarithm", "modulo"):
+        x = str(input("Enter first number: "))
+        y = str(input("Enter second number: "))
+
+        if x == "pi":
+            x = float(math.pi)
+
+        if y == "pi":
+            x = float(math.pi)
+
+        x = float(x)
+        y = float(y)
+
+        if choice == "addition":
+            print(" ", x, "+", y, "=", add(x, y))
+
+        elif choice == "subtraction":
+            print(" ", x, "-", y, "=", subtract(x, y))
+
+        elif choice == "multiplication":
+            print(" ", x, "*", y, "=", multiply(x, y))
+
+        elif choice == "division":
+            print(" ", x, "/", y, "=", divide(x, y))
+
+        elif choice == "exponents":
+            print(" ", x, "^", y, "=", exponent(x, y))
+
+        elif choice == "nth root":
+            print(" ", "^", x, "√", y, "=", nthroot(x, y))
+
+        elif choice == "logarithm":
+            print(" ", f'log({x} [, {y}]) = ', log(x, y)) #THANK YOU CAFF!!!
+
+        elif choice == "modulo":
+            print(" ", x, "%", y, "=", modulo(x, y))
+
+        else:
+            print("Invalid Input")
+
+
+#1 choice ####################
+    if choice in ("square root", "sine", "cosine", "tangent", "cotangent", "secant"):
+        x = float(input("Enter first number: "))
+
+        if choice == "square root":
+            print(" ","√", x, "=", sqrt(x))
+
+        elif choice == "sine":
+            print(" ", f'sin({x}) = ', sine(x))
+
+        elif choice == "cosine":
+            print(" ", f'cos({x}) = ', cosine(x))
+
+        elif choice == "tangent":
+            print(" ", f'tan({x}) = ', tangent(x))
+
+        elif choice == "cotangent":
+            print(" ", f'cot({x}) = ', cotangent(x))
+
+        elif choice == "secant":
+            print(" ", f'sec({x}) = ', secant(x))
+
+        else:
+            print("Invalid Input")
